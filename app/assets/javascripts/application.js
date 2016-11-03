851191a6481d6759f94ab8242b1aa7d776a2b583
aaron foster bresley <afbresley@gmail.com>
2016-11-03 01:21:03 -0400

    format and new copy

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,5 @@
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require turbolinks
+//= require mine.js
 //= require_tree .
---
+++ app/assets/javascripts/mine.js
@@ -0,0 +1,10 @@
+$(document).ready( function () {
+
+  $('a.btn-fugu').click( function(){ //you can give id or class name here for $('button')
+      $(this).text(function(i,old){
+          console.log(old)
+          return $.trim(old) == 'hide application' ?  'OPEN APPLICATION' : 'hide application';
+      });
+  });
+
+})
