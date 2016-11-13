169599c8bff3e7cef7d5aa64b5118e216bba9ea0
aaron foster bresley <afbresley@gmail.com>
2016-11-13 17:57:25 -0500

    allows deletion

--- app/controllers/coincidents_controller.rb
+++ app/controllers/coincidents_controller.rb
@@ -17,6 +17,14 @@ class CoincidentsController < ApplicationController
   def show
   end
 
+  def destroy
+    @coincident = Coincident.find(params[:id])
+    if @coincident.destroy
+      # flash[:notice] = t('success', obj: 'meow meow', past_participle: 'deleted')
+    end
+    redirect_to admin_path
+  end
+
   protected
 
     def coincident_params
