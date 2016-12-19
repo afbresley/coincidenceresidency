637cdfc00b00beace6f32258e8d3e20a738bdfe4
aaron foster bresley <afbresley@gmail.com>
2016-12-18 21:29:44 -0500

    hides applications

--- app/assets/javascripts/mine.js
+++ app/assets/javascripts/mine.js
@@ -3,7 +3,7 @@ $(document).ready( function () {
   $('a.btn-fugu').click( function(){ //you can give id or class name here for $('button')
       $(this).text(function(i,old){
           console.log(old)
-          return $.trim(old) == 'hide application' ?  'OPEN APPLICATION' : 'hide application';
+          // return $.trim(old) == 'hide application' ?  'OPEN APPLICATION' : 'hide application';
       });
   });
 
