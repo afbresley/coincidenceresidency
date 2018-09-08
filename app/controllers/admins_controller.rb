07a5a391270a730374c8eb30ca096b9e939053f9
aaron foster bresley <afbresley@gmail.com>
2018-09-08 08:38:30 -0400

    updates admin portal for third year

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -11,8 +11,8 @@ class AdminsController < ApplicationController
 
     # @coincidents = Coincident.all.order(:name)
 
-    @coincidents = CoincidentTwo.all.order(:name)
-    render 'admin/show', admin: current_admin.id, @coincidents => @coincidents
+    @coincidents = CoincidentTroi.all.order(:name)
+    render 'admin/show2', admin: current_admin.id, @coincidents => @coincidents
   end
 
 end
